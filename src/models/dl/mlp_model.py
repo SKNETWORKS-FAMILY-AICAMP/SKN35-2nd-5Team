69aa44b473f80e9933f2583b08472fb044bb6a9e
chan <pcr0807@naver.com>
2026-08-25 11:33:26 +0900

    [refactor] Remove comments

--- src/models/dl/mlp_model.py
+++ src/models/dl/mlp_model.py
@@ -21,22 +21,3 @@ class MLPClassifier(nn.Module):
 
     def forward(self, x):
         return self.feature_extractor(x)
-
-
-# Optuna trial 객체를 받아 탐색용 임시 MLP 구조를 생성하는 함수
-# def build_trial_model(trial, in_features):
-#     n_layers = trial.suggest_int("n_layers", 1, 3)
-#     layers = []
-
-#     for i in range(n_layers):
-#         out_features = trial.suggest_int(f"n_units_l{i}", 16, 128)
-#         layers.append(nn.Linear(in_features, out_features))
-#         layers.append(nn.BatchNorm1d(out_features))
-#         layers.append(nn.ReLU())
-
-#         dropout_rate = trial.suggest_float(f"dropout_l{i}", 0.1, 0.5)
-#         layers.append(nn.Dropout(dropout_rate))
-#         in_features = out_features
-
-#     layers.append(nn.Linear(in_features, 1))
-#     return nn.Sequential(*layers)
